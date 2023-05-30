4ba398ae20b52601bfc6feb1d22aa596637a591f
bakastrumpf <baka.strumpf@gmail.com>
2023-05-30 22:40:17 +0200

    combining various projects codes

--- index.php
+++ index.php
@@ -8,17 +8,35 @@
     <meta name="description" content="PHP: Library" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/styles-index.css">
     <title>PHP | LIBRARY</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
 
-    Hello, library!
-
     <div id="wrapper">
 
         <header>
+            <div id="header">
+                <div class="topMenu">
+                    <a href="index.php">Home page</a> |
+                    <a href="news.php">News</a> |
+                    <a href="search.php">Book search</a> |
+                    <a href="contact.php">Contact</a> |
+                    <a href="login.php">Login</a>
+                </div>
+
+                <div class="ime">
+                    <h1>Welcome to our library!
+                    </h1>
+                    <p>So glad to have you here. </p>
+                    <hr>
+                </div>
+            </div>
 
         </header>
 
