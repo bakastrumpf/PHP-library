67d7cfe9eb3e9c04740826f8841ac5d5d1d10544
bakastrumpf <baka.strumpf@gmail.com>
2023-05-30 21:24:52 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="sr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Prekvalifikacije, ETF, PHP, html5, css, bootstrap" />
+    <meta name="author" content="MS" />
+    <meta name="description" content="PHP: Library" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <title>PHP | LIBRARY</title>
+</head>
+
+<body>
+
+    Hello, library!
+
+    <div id="wrapper">
+
+        <header>
+
+        </header>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+        <footer>
+
+        </footer>
+
+
+    </div>
+
+</body>
+
+</html>
