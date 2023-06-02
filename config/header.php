2b45cf594915624054356976e5079074e63a1e24
bakastrumpf <baka.strumpf@gmail.com>
2023-06-02 23:59:03 +0200

    no change

--- config/header.php
+++ config/header.php
@@ -1,7 +1,9 @@
 <div class="topMenu">
+
     <a href="index.php">Home page</a> |
     <a href="pages/news.php">News</a> |
     <a href="pages/search.php">Book search</a> |
     <a href="pages/contact.php">Contact</a> |
     <a href="users/login.php">Login</a>
+
 </div>
