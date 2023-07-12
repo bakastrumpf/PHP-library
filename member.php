bda87642f11246188807ebcdbdb9a124d06dbffb
bakastrumpf <baka.strumpf@gmail.com>
2023-07-12 22:18:05 +0200

    member update | working on rentals history display

--- member.php
+++ member.php
@@ -14,7 +14,10 @@ if ($_SESSION['user_role'] == 'LIBRARIAN') {
 }
 ?>
 
-<?php include('header.php'); ?>
+<?php
+include('header.php');
+include_once('config.inc.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,7 +45,6 @@ if ($_SESSION['user_role'] == 'LIBRARIAN') {
     <div style="float:right">
         <a href="user-settings.php">SETTINGS</a> |
         <a href="logout.php">LOGOUT</a>
-
     </div>
 
     <ul class="nav nav-tabs" id="membMenu" role="tablist">
@@ -59,10 +61,13 @@ if ($_SESSION['user_role'] == 'LIBRARIAN') {
             <button class="nav-link" id="reviews-tab" data-bs-toggle="tab" data-bs-target="#reviews-tab-pane" type="button" role="tab" aria-controls="reviews-tab-pane" aria-selected="false">Reviews</button>
         </li>
     </ul>
+
     <div class="tab-content" id="membMenuContent">
+
         <div class="tab-pane fade show active" id="searchBooks-tab-pane" role="tabpanel" aria-labelledby="searchBooks-tab" tabindex="0">
             <?php include('search.php'); ?>
         </div>
+
         <div class="tab-pane fade" id="reservation-tab-pane" role="tabpanel" aria-labelledby="reservation-tab" tabindex="0">
             DISPLAY RESERVATIONS | TO BE COMPLETED
             <br>
@@ -71,14 +76,82 @@ if ($_SESSION['user_role'] == 'LIBRARIAN') {
             "SELECT BOOKtitle FROM RESERVATION WHERE idBOOK = "
             <br>
         </div>
+
         <div class="tab-pane fade" id="history-tab-pane" role="tabpanel" aria-labelledby="history-tab" tabindex="0">
-            DISPLAY RENTALS HISTORY | TO BE COMPLETED
+            <!-- DISPLAY RENTALS HISTORY | TO BE COMPLETED
             <br>
             Iz tabele BORROWED uraditi
             <br>
             "SELECT * FROM BORROWED WHERE idBOOK = "
-            <br>
+            <br> -->
+
+            <p>On this page, you can see your book rentals history.</p>
+            <form name="history" id="history" method="GET" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                <p>Results sorted:
+                    <select name="sort">
+                        <option selected>ascending</option>
+                        <option>descending</option>
+                    </select>
+                </p>
+                <br>
+                <hr>
+                <input type="submit" name="history" value="SHOW HISTORY">
+            </form>
+
+
+            <?php
+            if (isset($_GET['history'])) {
+
+                $iduser = $_SESSION['idUSER'];
+
+                $idBOOK = $_GET['id'];
+
+                $sql = "SELECT USERname, USERsurname, BOOKtitle, AUTHORname, AUTHORsurname, BORRdateTaken, BORRdateRet 
+                        FROM BORROWED bd, USER usr, BOOK bk, AUTHOR ath, WRITTEN wtn 
+                        WHERE usr.idUSER = bd.USER_idUSER 
+                            AND bk.idBOOK = bd.BOOK_idBOOK 
+                            AND ath.idAUTHOR = wtn.AUTHOR_idAUTHOR
+                        ORDER BY USERsurname ASC";
+
+                echo ("SUCCESS! ");
+                $result = mysqli_query($conn, $sql)
+                    or die("Error: " . mysqli_error($conn));
+
+
+                if (mysqli_num_rows($result) > 0) {
+                    // results render
+                    echo "<table border='5'>";
+                    echo "<tr>";
+                    echo "<th>Title</th>";
+                    echo "<th>Author surname</th>";
+                    echo "<th>Author name</th>";
+                    echo "<th>Taken</th>";
+                    echo "<th>Returned</th>";
+                    echo "<tr>";
+
+                    while ($row = mysqli_fetch_array($result)) {
+                        echo "<tr>";
+                        echo "<td>{$row['BOOKtitle']}</td>";
+                        echo "<td>{$row['AUTHORsurname']}</td>";
+                        echo "<td>{$row['AUTHORname']}</td>";
+                        echo "<td>{$row['BORRdateTaken']}</td>";
+                        echo "<td>{$row['BORRdateRet']}</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<br> No results fetched. 
+                    <br> You haven't been reading lately, have you? 
+                    <br> How about browsing for books to see if you'd like something, 
+                    <br> or heading to the library directly instead? 
+                    <br> So far, those are the best ways to populate this page with data :wink: ";
+                }
+                mysqli_free_result($result);
+                mysqli_close($conn);
+            }
+            ?>
         </div>
+
         <div class="tab-pane fade" id="reviews-tab-pane" role="tabpanel" aria-labelledby="reviews-tab" tabindex="0">
             REVIEWS | TO BE COMPLETED
             <br>
